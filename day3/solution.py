78c546738a63070edc52d68526bcbe2650c451a1
Kyle Charlton <kyle@kcharlton.dev>
2024-12-03 16:10:58 -0700

    Finished Day 3

--- day3/solution.py
+++ day3/solution.py
@@ -11,7 +11,7 @@ for line in lines:
     i = 0
     while i < len(line):
         if line[i:].startswith("mul("):
-            start = i+4
+            start = i + 4
             j = start
             end = -1
             while j < len(line):
@@ -25,7 +25,41 @@ for line in lines:
                 tokens = capture.split(",")
                 if (len(tokens) == 2 and re.match(r"^\d{1,3}$", tokens[0]) and re.match(r"^\d{1,3}$", tokens[1])):
                     total += int(tokens[0]) * int(tokens[1])
-                i = i+4
-        i += 1
+                i += 4
+        else:
+            i += 1
 
 print(total)
+
+total2 = 0
+
+do = True
+for line in lines:
+    i = 0
+    while i < len(line):
+        if do and line[i:].startswith("mul("):
+            start = i+4
+            j = start
+            end = -1
+            while j < len(line):
+                if line[j] == ")":
+                    end = j
+                    break
+                j += 1
+
+            if end != -1:
+                capture = line[start:end]
+                tokens = capture.split(",")
+                if (len(tokens) == 2 and re.match(r"^\d{1,3}$", tokens[0]) and re.match(r"^\d{1,3}$", tokens[1])):
+                    total2 += int(tokens[0]) * int(tokens[1])
+                i += 4
+        elif not do and line[i:].startswith("do()"):
+            do = True
+            i += 4
+        elif do and line[i:].startswith("don't()"):
+            do = False
+            i += 7
+        else:
+            i += 1
+
+print(total2)
