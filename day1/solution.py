d516c6f5adb61bbf5a81752eea8ea543583692c7
Kyle Charlton <kyle@kcharlton.dev>
2024-12-02 18:40:07 -0700

    Finished Day 1

--- day1/solution.py
+++ day1/solution.py
@@ -10,8 +10,15 @@ with open('input.txt') as f:
 list1.sort()
 list2.sort()
 
-sum = 0
+sum1 = 0
 for i in range(len(list1)):
-    sum += abs(list1[i] - list2[i])
+    sum1 += abs(list1[i] - list2[i])
 
-print(sum)
+print(sum1)
+
+sum2 = 0
+for item in list2:
+    if item in list1:
+        sum2 += item
+
+print(sum2)
